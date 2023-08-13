e24b8959d7bcdefe57aeaad69eac576e5fec591d
YeGoTaChi <yego.designs@gmail.com>
2023-08-13 22:54:04 +0100

    Done maybe, Added ReadMe

---
+++ README.md
@@ -0,0 +1,12 @@
+## What is this ?
+A Tool to convert a dotFile to Png
+instead of using `dot` executable
+
+## Why ?
+Sometimes you dont have root to install Executables
+Or maybe you just need it for one time
+
+## How to use ?
+```console
+$ node index.js [height] [dotfile] [output.png]
+```
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+#!/usr/local/bin/node
 import { instance } from "@viz-js/viz";
 import cstp from "convert-svg-to-png";
-import { writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { argv, exit } from "process";
 
+if (argv.length != 5)
+{
+	console.log("Usage: node index.js [height] [dotfile] [output.png]")
+	exit(0)
+}
+
+const height = parseInt(argv[2])
+const dotFilename = argv[3]
+const outputFilename = argv[4]
+
+if (!existsSync(dotFilename))
+{
+	console.error(`File ${dotFilename} does not exist !`);
+	exit(-1)
+}
 
 instance().then(async (viz) => {
-	const svg = viz.render("digraph { a -> b }", {format: "svg"});
-	console.log(svg.output);
+	console.log("Generating...")
 
-	const png = await cstp.convert(svg.output, {height: 1000});
-	writeFileSync("file.png", png);
+	const dotData = readFileSync(dotFilename)
+
+	const svg = viz.render(dotData.toString(), {format: "svg"});
+	const png = await cstp.convert(svg.output, {height});
+
+	writeFileSync(outputFilename, png);
+
+	console.log("Done !")
 });
